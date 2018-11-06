eb3c26d8c56e047a25c12eb4f61b76f20d84eb27
George Mitsikaris <30596553+GeorgeMits@users.noreply.github.com>
2018-11-06 21:51:54 +0200

    Creating Partial Views For Buttons

---
+++ MVCMemberships/Areas/Admin/Models/SmallButtonModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace MVCMemberships.Areas.Admin.Models
+{
+    public class SmallButtonModel
+    {
+        public string Action { get; set; }
+        public string Text { get; set; }
+        public string Glyph { get; set; }
+        public string ButtonType { get; set; }
+        public int? Id { get; set; }
+        public int? ItemId { get; set; }
+        public int? ProductId { get; set; }
+        public int? SubscriptionId { get; set; }
+        public string ActionParameters {
+            get
+            {
+                StringBuilder sb = new StringBuilder("?");
+                if (Id > 0 && Id != null) 
+                    sb.Append(string.Format($"Id={Id}&"));
+                if (ItemId > 0 && ItemId != null)
+                    sb.Append(string.Format($"ItemId={ItemId}&"));
+                if (ProductId > 0 && ProductId != null)
+                    sb.Append(string.Format($"ProductId={ProductId}&"));
+                if (SubscriptionId > 0 && SubscriptionId != null)
+                    sb.Append(string.Format($"SubscriptionId={SubscriptionId}"));
+                return sb.ToString();
+            }
+        }
+    }
+}
